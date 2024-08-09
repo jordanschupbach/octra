abcef0b7a355eab1f8abef96db65b9bce80b7d0f
jordans1882 <jordans1882@gmail.com>
2024-08-09 09:16:48 -0600

    Update octrajs paths

--- prebindings/octrajs/lib/index.js
+++ prebindings/octrajs/lib/index.js
@@ -1,3 +1,8 @@
 export const helloWorld = function () {
   console.log("Hello, world!");
 };
+
+var example = import("./build/Release/octrajs");
+x = new example.DynArray(10);
+x.set(0, 1.1);
+console.log(x.get(0));
